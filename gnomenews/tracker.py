a00bf9cd55d67b4addd9f5888201562b87da78b6
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-07-20 18:02:02 +0200

    tracker: don't load posts twice after post was marked as read

--- gnomenews/tracker.py
+++ gnomenews/tracker.py
@@ -138,7 +138,6 @@ class Tracker(GObject.GObject):
 
         logger.debug(query)
         self.sparql.update(query, GLib.PRIORITY_DEFAULT, None)
-        self.emit('items-updated')
 
     @log
     def remove_channel(self, url):
